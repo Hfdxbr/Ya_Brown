4e1181358da619d5eb7b237ca0896084e64dcbf9
Ravil Shabaykin <rshabaykin@aligntech.com>
2020-02-07 12:23:37 +0300

    Трекер задач (update)

--- 54_task_manager/TeamTasks.cpp
+++ 54_task_manager/TeamTasks.cpp
@@ -1,5 +1,4 @@
 #include "TeamTasks.h"
-#include <vector>
 
 const TasksInfo& TeamTasks::GetPersonTasksInfo(
     const std::string& person) const {
@@ -19,26 +18,30 @@ void TeamTasks::AddNewTask(const std::string& person) {
 
 std::tuple<TasksInfo, TasksInfo> TeamTasks::PerformPersonTasks(
     const std::string& person, int task_count) {
-  const std::vector<TaskStatus> workflow = {
-      TaskStatus::NEW, TaskStatus::IN_PROGRESS, TaskStatus::TESTING, TaskStatus::DONE};
-  auto stage = workflow.begin();
-
   TasksInfo& person_tasks = tasks_by_user[person];
   TasksInfo changed;
   TasksInfo unchanged;
 
-  auto done_on_prev_stage = 0;
-  while (stage != --workflow.end()) {
-    auto done_on_stage = task_count > person_tasks[*stage] ? person_tasks[*stage] : task_count;
-    auto left_on_stage = person_tasks[*stage] - done_on_stage - done_on_prev_stage;
-    task_count -= done_on_stage;
-    person_tasks[*stage] -= done_on_stage;
-    if (left_on_stage) unchanged[*stage] = left_on_stage;
-    ++stage;
-    done_on_prev_stage = done_on_stage;
-    person_tasks[*stage] += done_on_stage;
-    if (done_on_stage) changed[*stage] += done_on_stage;
+  while (task_count--) {
+    if (person_tasks[TaskStatus::NEW]) {
+      ++changed[TaskStatus::IN_PROGRESS];
+      --person_tasks[TaskStatus::NEW];
+    } else if (person_tasks[TaskStatus::IN_PROGRESS]) {
+      ++changed[TaskStatus::TESTING];
+      --person_tasks[TaskStatus::IN_PROGRESS];
+    } else if (person_tasks[TaskStatus::TESTING]) {
+      ++changed[TaskStatus::DONE];
+      --person_tasks[TaskStatus::TESTING];
+    } else
+      break;
   }
+
+  for(const auto&[stage, num] : person_tasks)
+    if(num > 0 && stage != TaskStatus::DONE)
+      unchanged[stage] = num;
+
+  for(const auto&[stage, num] : changed)
+      person_tasks[stage] += num;
 
   return {changed, unchanged};
 }
